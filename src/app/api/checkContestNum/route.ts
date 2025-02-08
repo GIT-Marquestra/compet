2ceb44bd37112eaaddc6cb171f4c13f640c2e6c8
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2025-02-08 23:56:40 +0530

    deploying to prod

--- src/app/api/checkContestNum/route.ts
+++ src/app/api/checkContestNum/route.ts
@@ -4,15 +4,19 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     try {
         const lastContestNum = await prisma.contest.findFirst({
-            orderBy:{
+            orderBy: {
                 createdAt: "desc"
             }
-        })
-        if(!lastContestNum) return
-        return NextResponse.json({ lastContestNum.id }, { status: 200 });
+        });
+
+        if (!lastContestNum) {
+            return NextResponse.json({ error: "No contests found" }, { status: 404 });
+        }
+
+        return NextResponse.json({ id: lastContestNum.id }, { status: 200 });
+
     } catch (error) {
-        // @ts-ignore
-        console.log(error.message)
-        return NextResponse.json({ error: "Error fetchig last contest number" }, { status: 400 })
+        console.error("Error fetching last contest number:", error);
+        return NextResponse.json({ error: "Error fetching last contest number" }, { status: 400 });
     }
 }
